31a9033900e08402db133321195cb01219422982
shiramwangi <shiramwangi69@gmail.com>
2025-09-22 00:42:05 +0300

    Initial commit: 3Design project setup with Next.js, Tailwind CSS, and shadcn/ui components

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import CubeGenerator from "../cube-generator"
+import EnvMapGenerator from "../env-map-generator"
+
+export default function Page() {
+  return (
+    <div>
+      <CubeGenerator />
+      <EnvMapGenerator />
+    </div>
+  )
+}
